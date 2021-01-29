e4e15e8d59dc62e9bbd31d8b13af0c956d74443c
Everton Natã <37844708+evertonnatan@users.noreply.github.com>
2021-01-29 07:10:37 -0300

    Adicionando a estrutura de controle

--- scripts.js
+++ scripts.js
@@ -28,6 +28,12 @@ const Modal = {
       amount: -20000,
       date: '23/01/2021'
 },
+{
+    id: 4,
+    description: 'App',
+    amount: 20000,
+    date: '23/01/2021'
+},
 ]
 
   const Transaction = {
@@ -51,21 +57,46 @@ const Modal = {
 
         DOM.transactionsContainer.appendChild(tr)
       },
+
       innerHTMLTransaction(transaction) {
+          const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+          const amount = Utils.formatCurrency(transaction.amount)
 
 
           const html = `
               <td class="description">${transaction.description}</td>
-              <td class="expense">${transaction.amount}</td>
+              <td class="${CSSclass}">${amount}</td>
               <td class="date">${transaction.date}</td>
               <td><img src="./assets/minus.svg" alt="Remover transação"></td>
               `
 
               return html
+      },
+
+      updateBalance() {
+        document.getElementById('incomeDisplay').innerHTML = Transaction.incomes()
+        document.getElementById('expenseDisplay').innerHTML = Transaction.expenses()
+        document.getElementById('totalDisplay').innerHTML = Transaction.total()
       }
   }
 
-  DOM.addTransaction(transactions[2])
+  const Utils = {
+    formatCurrency(value) {
+      const signal = Number(value) < 0 ? "-" : ""
 
-  transactions.forEach(function(transaction) {})
+      value = String(value).replace(/\D/g, "")
+      value = Number(value) / 100
+      value = value.toLocaleString("pt-BR", { style: "currency", currency: "BRL"})
 
+      return signal + value
+    }
+  }
+
+  transactions.forEach(function(transaction) {
+      DOM.addTransaction(transaction)
+  })
+
+  DOM.updateBalance()
+
+ 
